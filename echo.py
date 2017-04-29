326fb6359fe45b018ddaa68bf098dd97152e8c48
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2017-04-29 14:51:21 +0530

    Added functionality so that bot can proactively send me message.

--- echo.py
+++ echo.py
@@ -1,11 +1,18 @@
 from flask import Flask, request
+from pymessenger.bot import Bot
 import requests
 import json
 import traceback
 import random
 app = Flask(__name__)
 # Raula machega
-token = "EAAF96u7EUXIBAL9sskjA1ZC3UyNwEjby116a3wBIFJLdoTvA02S3xrTj3sTAvZAuKWd8isVRknXswxSjGGFkrvCI4YdLngybZCgB0K7BnIZCFhJAxdqluFSruEPQQhVdZCaB1AoxDtNKyOxacUQfvPIL92zziOZAWCyq2ouxtYbQZDZD"
+access_token = "EAAF96u7EUXIBAL9sskjA1ZC3UyNwEjby116a3wBIFJLdoTvA02S3xrTj3sTAvZAuKWd8isVRknXswxSjGGFkrvCI4YdLngybZCgB0K7BnIZCFhJAxdqluFSruEPQQhVdZCaB1AoxDtNKyOxacUQfvPIL92zziOZAWCyq2ouxtYbQZDZD"
+recipient_id="1442034082522401"
+
+bot=Bot(access_token)
+message="Aur bot kaise ho?"
+
+bot.send_text_message(recipient_id,message)
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
@@ -15,7 +22,7 @@ def webhook():
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
       payload = {'recipient': {'id': sender}, 'message': {'text': sender}} # We're going to send this back
-      r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
+      r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, json=payload) # Lets send it
     except Exception as e:
       print traceback.format_exc() # something went wrong
   elif request.method == 'GET': # For the initial verification
