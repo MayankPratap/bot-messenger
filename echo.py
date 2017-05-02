387ab39c211401835e993d11c06e7393c30cc1e3
Mayank Pratap <mayank7860@googlemail.com>
2017-05-02 23:38:34 +0530

    Working code to be deployed on heroku, since ngrok not working

--- echo.py
+++ echo.py
@@ -88,13 +88,13 @@ def webhook():
 			#data = json.loads(request.data)
 			text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
 			sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-			payload = {'recipient': {'id': sender}, 'message': {'text': sender}} # We're going to send this back
+			#payload = {'recipient': {'id': sender}, 'message': {'text': sender}} # We're going to send this back
 			#r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, json=payload) # Lets send it
 			
 		except Exception as e:
 			print(traceback.format_exc()) # something went wrong
 	elif request.method == 'GET': # For the initial verification
-		if request.args.get('hub.verify_token') == 'iamsherlock':
+		if request.args.get('hub.verify_token') == 'marksuckerberg':
 			return request.args.get('hub.challenge')
 		return "Wrong Verify Token"
 	return "Everything's OK"
